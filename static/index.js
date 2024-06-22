c67d9ebf6c45442ea5d15f2e62eb904b24c4eae3
LeQuangDaiDi <dile8861@gmail.com>
2024-06-22 13:25:06 +0700

    update

--- static/index.js
+++ static/index.js
@@ -1,18 +1,35 @@
-const {createApp} = Vue;
+const { createApp } = Vue;
 
 const app = createApp({
-    data(){
+    data() {
         return {
             lang: 'vi',
             i18n: {
                 'en': {
-                    'test': 'Test'
+                    'home': 'Home',
+                    'content-1': 'OUR STUDIO'
                 },
                 'vi': {
-                    'test': 'cc'
+                    'home': 'Trang Chủ',
+                    'content-1': 'OUR STUDIO'
                 }
             }
         }
+    },
+    methods: {
+        getImage(link) {
+            return link;
+        },
+        getContent(content) {
+            return this.i18n[this.lang][content];
+        },
+        switchLanguage() {
+            if (this.lang === 'vi') {
+                this.lang = 'en';
+            } else {
+                this.lang = 'vi';
+        }
     }
-});
-app.mount('#app')
+    
+}});
+app.mount('#app');
